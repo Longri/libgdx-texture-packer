fee5c14ef5e88855abd434c6e0643dd9cecce333
Longri <www.longri.de@googlemail.com>
2018-02-15 13:41:17 +0100

    include rectpack2D

--- LibPP/src/de/longri/libPP/NativePacker.java
+++ LibPP/src/de/longri/libPP/NativePacker.java
@@ -20,6 +20,92 @@ package de.longri.libPP;
  */
 public class NativePacker {
 
+    //@off
+    /*JNI
+
+    #include "pack.h"
+    #include <cstring>
+    #include <algorithm>
+
+    void packRecArray(short *valueArray, int count, int maxTexSize, bool allowFlipp, bool debug) {
+
+
+    rect_xywhf rects[count], *ptr_rects[count];
+
+    int index = 0;
+    for (int i = 0; i < count; ++i) {
+        rects[i] = rect_xywhf(valueArray[index + 0], //index
+                              valueArray[index + 1], // x
+                              valueArray[index + 2], // y
+                              valueArray[index + 3], // width
+                              valueArray[index + 4]); // height
+        ptr_rects[i] = rects + i;
+        index += 7;
+    }
+
+    vector<bin> bins;
+
+    if (pack(ptr_rects, count, maxTexSize, allowFlipp, bins)) {
+        if (debug) {
+            printf("bins: %d\n", bins.size());
+        }
+
+        for (int i = 0; i < bins.size(); ++i) {
+            if (debug) {
+                printf("\n\nbin: %dx%d, rects: %d\n", bins[i].size.w, bins[i].size.h, bins[i].rects.size());
+            }
+
+            for (int r = 0; r < bins[i].rects.size(); ++r) {
+                rect_xywhf *rect = bins[i].rects[r];
+
+                int recIndex = rect->index * 7;
+                valueArray[recIndex + 1] = static_cast<short>(rect->x);
+                valueArray[recIndex + 2] = static_cast<short>(rect->y);
+                valueArray[recIndex + 3] = static_cast<short>(rect->w);
+                valueArray[recIndex + 4] = static_cast<short>(rect->h);
+                valueArray[recIndex + 5] = static_cast<short>(rect->flipped ? 1 : 0);
+                valueArray[recIndex + 6] = static_cast<short>(i);
+
+                if (debug) {
+                    printf("REC index: %d x: %d, y: %d, w: %d, h: %d, was flipped: %s\n", rect->index, rect->x,
+                           rect->y, rect->w, rect->h,
+                           rect->flipped ? "yes" : " no");
+                }
+            }
+        }
+
+        if (debug) {
+            printf("\n Array result:\n");
+
+
+            //print ValueArray
+            index = 0;
+            for (int i = 0; i < count; ++i) {
+                printf("REC index: %d x: %d, y: %d, w: %d, h: %d, was flipped: %s textureIndex:  %d \n" //
+                        , valueArray[index + 0] // index
+                        , valueArray[index + 1] // x
+                        , valueArray[index + 2] // y
+                        , valueArray[index + 3] // width
+                        , valueArray[index + 4] // height
+                        , valueArray[index + 5] > 0 ? "yes" : " no" // flipped
+                        , valueArray[index + 6]); // texture index
+
+                index += 7;
+            }
+        }
+    } else {
+        printf("failed: there's a rectangle with width/height bigger than max_size!\n");
+    }
+}
+
+
+
+
+
+     */
+
+
+
     public static native int[] packNative(short[] valueArray, int maxTextureSize, boolean allowFlip, boolean writeDebug); /*
     jintArray result;
     int size =3;
